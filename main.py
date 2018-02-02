0293c54984f427c1c88b846b9695424e44b3d4cf
Sacasa <mrsacasagaming@gmail.com>
2018-02-02 17:34:06 +0100

    Not quite working yet

---
+++ main.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import time
+import numpy as np
+import sys
+import preprocessing
+import seams
+
+if __name__ == '__main__':
+    start = int(round(time.time() * 1000))
+    name = sys.argv[1]
+    im = Image.open(name)
+    cop = im.convert("L")
+    pix = im.load()
+    img = im.copy()
+
+    for i in range(60):
+        print("{} ème iteration".format(i))
+        gradient = preprocessing.energy_map(img)
+        list_sims_coords = seams.dynamic_programming(gradient)
+        img = seams.move(img,list_sims_coords)
+
+    end = int(round(time.time() * 1000))
+    print("Temps d'execution : {} ms".format(end-start))
+    print("Taille originale : ({},{})".format(im.size[0],im.size[1]))
+    print("Taille originale : ({},{})".format(img.size[0],img.size[1]))
+
+    img.show()
---
+++ preprocessing.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import math
+
+def energy_map(im):
+    conv1 = [[-1,0,1],[-2,0,2],[-1,0,1]]
+    conv2 = [[-1,-2,-1],[0,0,0],[1,2,1]]
+    cop = im.convert("L")
+    pix = cop.load()
+    map_ener = [[0]*im.size[0] for y in range(im.size[1])]
+    for x in range(im.size[0]):
+        for y in range(im.size[1]):
+            voisinnage = [[0,0,0],[0,0,0],[0,0,0]]
+            i=0
+            for yp in range(y-1,y+2):
+                j=0
+                for xp in range(x-1,x+2):
+                    if 0<=xp<im.size[0] and 0<=yp<im.size[1]:
+                        v = pix[xp,yp]
+                        voisinnage[j][i]=v
+                    j+=1
+                i +=1
+
+            Gx=convo3x3(conv1,voisinnage)
+            Gy=convo3x3(conv2,voisinnage)
+            G=math.sqrt(Gx**2+Gy**2)
+            map_ener[y][x]=int(G)
+    return map_ener
+
+
+def convo3x3(A,B):
+    tot = 0
+    for j in range(3):
+        for i in range(3):
+            tot += A[i][j]*B[i][j]
+    return tot
