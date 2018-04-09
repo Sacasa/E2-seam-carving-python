58b54893f3fda3fb2cc27344f494e8883b2737b0
garridoq <mrsacasagaming@gmail.com>
2018-04-09 13:54:08 +0200

    Sobel points

--- jit/preprocessing_jit.py
+++ jit/preprocessing_jit.py
@@ -28,6 +28,28 @@ def sobel(im_array,map_ener):
             map_ener[y][x]=int(G)
 
 @autojit
+def sobel_points(im_array,points,gradient):
+    convx = np.array([[-1,0,1],[-2,0,2],[-1,0,1]],dtype =np.int16)
+    convy = np.array([[-1,-2,-1],[0,0,0],[1,2,1]],dtype =np.int16)
+
+    #calculer sobel en x-1 et x uniquement
+    for point in points:
+        Gx=0
+        Gy=0
+        for xp in range(point[0]-1,point[0]+1):
+            if 0<=xp<x_max and 0<=yp<y_max: 
+                dx = xp - x + 1
+                dy = yp -y + 1
+                Gx += convx[dy][dx]*im_array[yp][xp]
+                Gy += convy[dy][dx]*im_array[yp][xp]
+        
+        G=math.sqrt(Gx**2+Gy**2)
+        G = int(G) if int(G) <=255 else 255
+        map_ener[point[1]][point[0]]=int(G)
+
+
+
+@autojit
 def sobel_v2(im_array,map_ener):
     convx = np.array([[-1,0,1],[-2,0,2],[-1,0,1]],dtype =np.int16)
     convy = np.array([[-1,-2,-1],[0,0,0],[1,2,1]],dtype =np.int16)
